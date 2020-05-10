a586552733c0be271ecc8c42cadd89bd68f21403
qikun <375723803@qq.com>
2020-05-10 21:08:22 +0800

    Initial commit

---
+++ src/main/java/com/jenkins/jekinsdemo/JekinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jekinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JekinsdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/jekinsdemo/JekinsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jekinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
